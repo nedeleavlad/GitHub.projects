fb3a199b5b9015b30b8e43172d177cf92ccfbb82
nedeleavlad <nedeleavlad28@outlook.com>
2015-12-05 23:10:27 +0200

    sportiv

---
+++ Sportiv/Sportiv/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sportiv
+{
+    [TestClass]
+    public class NumberRepetitionsTest
+    {
+        [TestMethod]
+        public void NumberRepetitions()
+        {
+            int number = CalculateRepetitions(3);
+
+            Assert.AreEqual(9, number);
+        }
+         
+        int CalculateRepetitions(int n)
+        
+            return (n(n+1)-n);
+        
+    }
+}
