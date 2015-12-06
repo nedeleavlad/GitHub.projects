585a117314b3ed447b1551bb57f7b091f7591eb8
nedeleavlad <nedeleavlad28@outlook.com>
2015-12-06 14:56:43 +0200

    problema ciuperci

---
+++ Ciuperci/Ciuperci/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ciuperci
+{
+    [TestClass]
+    public class CiuperciRosii
+    {
+        [TestMethod]
+        public void numarCiuperciRosii()
+        {
+            Assert.AreEqual(2, ciuperci_rosii(10, 3));
+
+        }
+
+        int ciuperci_rosii(int n,int x)
+            return ((x*n)/(x+1));
+    }
+}
