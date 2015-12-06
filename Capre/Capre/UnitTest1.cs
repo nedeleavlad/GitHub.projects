b2b7bbb6ad007d7051c8c9c1719dba22a42c85c2
nedeleavlad <nedeleavlad28@outlook.com>
2015-12-06 22:17:49 +0200

    problema capre

---
+++ Capre/Capre/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capre
+{
+    [TestClass]
+    public class KgFan
+    {
+        [TestMethod]
+        public void numarkgfan()
+        {
+            Assert.AreEqual(20, cateKgFan(3, 5, 6, 7, 7));
+        }
+
+            int cateKgFan(int x,int y,int z,int w,int q)
+        
+                return (x*q*w)/(z*y);
+    }
+}
