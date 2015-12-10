1620cea409dff8dc0a648a701f593612cf47cbf5
nedeleavlad <nedeleavlad28@outlook.com>
2015-12-10 12:11:29 +0200

    area_columns

---
+++ columns_area/columns_area/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace columns_area
+{
+    [TestClass]
+    public class RoomAreaTest
+    {
+        [TestMethod]
+        public void AreaTest()
+        {
+            Assert.AreEqual(6, CalculateArea(0, 0, 3, 0, 0, 4));
+
+        }
+
+        float CalculateArea(int xa,int ya,int xb,int yb,int xc,int yc)
+        {
+            float ab = sqrt(pow((xb - xa), 2) + pow((yb - ya), 2));
+
+            float bc = sqrt(pow((yc - yb), 2) + pow((xc - xb), 2));
+
+            float ac = sqrt(pow((yc - ya), 2) + pow((xc - xa), 2));
+
+            float p = (ab + ac + bc) / 2;
+
+            return sqrt(p * (p - ab) * (p - ac) * (p - bc));
+
+
+        }
+    }
+}
