a37aff0fa9f2886fc35a7dd2f88e63730aed35d6
nedeleavlad <nedeleavlad28@outlook.com>
2015-12-10 13:28:42 +0200

    pavement

---
+++ problema_pavaje_piata/problema_pavaje_piata/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace problema_pavaje_piata
+{
+    [TestClass]
+    public class Pavementt
+    {
+        [TestMethod]
+        public void Pavement()
+        {
+            Assert.AreEqual(5,CalculateNrPavage(5, 4, 2));
+
+        }
+
+        int CalculateNrPavage(int m,int n,int a)
+        {
+            int p = (m * n) / (a * a); //bucati intregi
+
+            return p;
+
+
+
+
+
+
+        }
+    }
+}
