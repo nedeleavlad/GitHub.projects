87ebf2fef762e932d2362d51debba2d6c445c8dc
nedeleavlad <nedeleavlad28@outlook.com>
2015-12-14 13:15:27 +0200

    train

---
+++ Train_problem/Train_problem/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Train_problem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BirdDistanceTest ()
+        {
+            Assert.AreEqual(25.5, CalculateDistanceBird(51));
+
+
+        }
+
+        double CalculateDistanceBird(double distance)
+        {
+            return distance / 2;
+
+
+        }
+    }
+}
