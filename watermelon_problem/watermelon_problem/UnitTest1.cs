f56f89ac2d07c298927f408e023389ca22d77595
nedeleavlad <nedeleavlad28@outlook.com>
2016-01-02 13:37:56 +0200

    watermelon_divide

---
+++ watermelon_problem/watermelon_problem/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace watermelon_problem
+{
+    [TestClass]
+    public class Watermelon
+    {
+        [TestMethod]
+        public void watermelonDivideTest1()
+        {
+            Assert.AreEqual(1, divide_watermelon(4));
+
+
+        }
+
+
+        [TestMethod]
+        public void watermelonDivideTest0()
+        {
+            Assert.AreEqual(0, divide_watermelon(3));
+
+
+        }
+
+
+        int divide_watermelon(int noKg)
+        {
+            if (noKg % 2 == 0) return 1;
+
+            else return 0;
+
+        }
+    }
+}
