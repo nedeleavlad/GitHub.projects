87bc8542cf03689c3769db4ae23da706f77fae66
nedeleavlad <nedeleavlad28@outlook.com>
2016-01-02 14:31:48 +0200

    problem_rent_resolved

--- rent_penalty/rent_penalty/UnitTest1.cs
+++ rent_penalty/rent_penalty/UnitTest1.cs
@@ -10,30 +10,37 @@ namespace rent_penalty
         public void rentPenalty10()
         {
 
-            Assert.AreEqual(11, calculateRent(10,10);
-        };
+            Assert.AreEqual(11, rentValue(10, 10));
+        }
 
 
         [TestMethod]
         public void rentPenalty30()
         {
 
-            Assert.AreEqual(15, calculateRent(10,25));
+            Assert.AreEqual(15, rentValue(10,25));
         }
 
         [TestMethod]
         public void rentPenalty40()
         {
 
-            Assert.AreEqual(45, calculateRent(10,35));
+            Assert.AreEqual(45, rentValue(10,35));
         }
 
-         double calculateRent(double rent, int noPenaltyDays)
+     double  rentValue (double rent, int noPenaltyDays)
 
         {
-            if ((1 <= noPenaltyDays) && (noPenaltyDays <= 10) return (rent + noPenaltyDays * 0.01*rent);
-            if ((11 <= noPenaltyDays) && (noPenaltyDays <= 30)   return (rent + noPenaltyDays * 0.02*rent);
-            if ((30 <= noPenaltyDays) && (noPenaltyDays <= 40) return (rent + noPenaltyDays * 0.1*rent);
+            if ((1 <= noPenaltyDays) && (noPenaltyDays <= 10)) return (rent + noPenaltyDays * 0.01 * rent);
+
+           
+
+                     else if ((11 <= noPenaltyDays) && (noPenaltyDays <= 30)) return (rent + noPenaltyDays * 0.02 * rent);
+
+
+                                  else  return (rent + noPenaltyDays * 0.1 * rent);
+
+            
         }
 
 
