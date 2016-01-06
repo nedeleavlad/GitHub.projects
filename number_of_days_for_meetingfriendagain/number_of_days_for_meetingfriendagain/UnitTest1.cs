89e30d269a302b4c1cc50f58e376efddb0622808
nedeleavlad <nedeleavlad28@outlook.com>
2016-01-06 14:18:43 +0200

    after how many days i will meet my friend again

---
+++ number_of_days_for_meetingfriendagain/number_of_days_for_meetingfriendagain/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace number_of_days_for_meetingfriendagain
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(12, meetingdays(4, 6));
+
+        }
+
+        int meetingdays(int me,int myfriend)
+        {
+            int product = me * myfriend;
+
+            while (me != myfriend)
+            {
+                if (me > myfriend)
+                    me = me - myfriend;
+                else
+                    myfriend = myfriend- me;
+            }
+
+            return product / me;
+
+        }
+
+
+    }
+}
