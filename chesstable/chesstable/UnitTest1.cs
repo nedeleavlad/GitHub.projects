d28fffa004bcf2c65830bd39a14ccc9debaf206a
nedeleavlad <nedeleavlad28@outlook.com>
2016-01-06 12:32:42 +0200

    chess table

---
+++ chesstable/chesstable/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace chesstable
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { Assert.AreEqual(204, nosquares(8));
+        }
+        
+
+        int nosquares(int n)
+        {
+            int i, number=0;
+
+            for(i=1;i<= n;i++)
+            {
+                number = number + i * i;
+
+
+            }
+
+            return number;
+        }
+
+    }
+}
