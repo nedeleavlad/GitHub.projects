da34973b18d933df490fbaf2b794b388d3579433
nedeleavlad <nedeleavlad28@outlook.com>
2016-01-06 13:29:52 +0200

    string prefix

---
+++ stringprefix/stringprefix/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace stringprefix
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("aa", prefix("aab","aaf"));
+
+        }
+        
+        string prefix(string s1, string s2)
+        {
+            string ss = string.Empty;
+            int i, j;
+
+               for(i=0;i<s1.Length;i++)
+                 {  for(j=0;j<s2.Length;j++)
+                       {   if (s1[i] == s2[j])
+
+                                  {
+                                    ss+=s2[j];
+                                   }
+                        }
+
+
+                  }
+            return ss;
+
+        }
+
+
+
+    }
+}
