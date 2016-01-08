16abf70d4a6cf1dcc06241bd6e24d3002bb591a2
nedeleavlad <nedeleavlad28@outlook.com>
2016-01-08 21:37:33 +0200

    panagram attempt

---
+++ it_is_or_not_panagram/it_is_or_not_panagram/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace it_is_or_not_panagram
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("true", findPanagram("The quick brown fox jumps over the lazy dog"));
+
+            
+        }
+
+        string findPanagram(string phrase)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+            int contor = 0;
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                for (int j = 0; j < phrase.Length; j++)
+                {
+                    if (alphabet[i] == phrase[j]) contor++;
+
+
+                }
+              
+
+           }
+
+            if (contor == alphabet.Length) return "true";
+            else return "false";
+
+
+        }
+
+
+    }
+}
