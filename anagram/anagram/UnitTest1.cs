5575a3cd12bd190fa0eaa96e18b4d870d012b19d
nedeleavlad <nedeleavlad28@outlook.com>
2016-01-14 13:27:56 +0200

    anagram

---
+++ anagram/anagram/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace anagram
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(6,CalculateAnagram("abc"));
+
+
+        }
+
+        int CalculateAnagram(string phrase)
+        {
+            int result = 1;
+
+
+            for(int i=1;i<=phrase.Length;i++)
+            {
+                result *= i;
+
+
+
+            }
+
+            return result;
+
+        }
+
+
+    }
+}
