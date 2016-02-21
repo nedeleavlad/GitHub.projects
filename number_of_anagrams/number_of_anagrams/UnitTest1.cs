6ac8b931fe3f63d66ebe97b633d41e04e390ec9d
nedeleavlad <nedeleavlad28@outlook.com>
2016-02-21 22:40:08 +0200

    number of anagrams

---
+++ number_of_anagrams/number_of_anagrams/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace number_of_anagrams
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(6, GetTheNumberOfAnagram("asd"));
+            Assert.AreEqual(30, GetTheNumberOfAnagram("aabbf"));
+        }
+
+        int GetTheNumberOfAnagram(string Word)
+        {
+            string LowerCase = Word.ToLower();
+            int result;
+            if (Word.Length == 0)
+                return 0;
+            result = GetTheFactorial(LowerCase.Length) / CountRepeatedChars(LowerCase);
+            return result;
+        }
+
+
+        int CountRepeatedChars(string string1)
+        {
+            int result = 0;
+            int p = 1;
+            for (char charToCheck = 'a'; charToCheck <= 'z'; charToCheck++)
+            {
+                result = CountChars(string1, charToCheck);
+                p *= GetTheFactorial(result);
+            }
+            return p;
+        }
+
+        private static int CountChars(string sstring, char charToCheck)
+        {
+            int result = 0;
+            for (int i = 0; i < sstring.Length; i++)
+            {
+                if (sstring[i] == charToCheck)
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+        int GetTheFactorial(int length)
+        {
+            int Number = 1;
+            for (int i = 1; i <= length; i++)
+            {
+                Number *= i;
+            }
+            return Number;
+        }
+
+
+
+
+
+
+    }
+}
