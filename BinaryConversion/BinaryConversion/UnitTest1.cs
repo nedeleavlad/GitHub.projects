126024ea2c6ac7778c33ff60ee6e8ba95be1207c
nedeleavlad <nedeleavlad28@outlook.com>
2016-03-01 23:22:31 +0200

    binary conversion not operation

--- BinaryConversion/BinaryConversion/UnitTest1.cs
+++ BinaryConversion/BinaryConversion/UnitTest1.cs
@@ -7,9 +7,9 @@ namespace BinaryConversion
     public class UnitTest1
     {
         [TestMethod]
-        public void BinaryConverionfor61()
+        public void BinaryConversionfor61()
         {
-            CollectionAssert.AreEqual(new byte[] { 1,1,1,1,0,1}, GetConversionToBinary(61));
+            CollectionAssert.AreEqual(new byte[] { 1, 1, 1, 1, 0, 1 }, GetConversionToBinary(61));
 
         }
 
@@ -24,19 +24,32 @@ namespace BinaryConversion
                 Array.Resize(ref byteArray, byteArray.Length + 1);
 
                 byteArray[contor] = (byte)(number % 2);
-
-
                 contor += 1;
-
                 number = number / 2;
-
-
             }
 
             Array.Reverse(byteArray);
 
             return byteArray;
+        }
 
+        [TestMethod]
+        public void OperationNOT()
+        {
+            CollectionAssert.AreEqual(new byte[] { 0, 0, 0, 0, 1, 0 }, GetOperationNOT(GetConversionToBinary(61)));
+
+        }
+
+        byte[] GetOperationNOT(byte[] byteArray)
+        {
+
+            for (int j = 0; j < byteArray.Length; j++)
+            {
+                if (byteArray[j] == 1) byteArray[j] = 0;
+                else byteArray[j] = 1;
+            }
+
+            return byteArray;
         }
 
 
