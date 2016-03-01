a581c2d2399d7e77aaa0805c9658d38544f4459b
nedeleavlad <nedeleavlad28@outlook.com>
2016-03-01 22:14:50 +0200

    byte conversion to base 2

---
+++ BinaryConversion/BinaryConversion/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//11001
+namespace BinaryConversion
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BinaryConverionfor61()
+        {
+            Assert.AreEqual(new byte[] { 1,1,1,1,0,1}, GetConversiontoBinary(61));
+
+        }
+
+        byte[] GetConversiontoBinary(int number)
+        {
+            byte[] byteArray = new byte[0];
+
+            int contor = 0;
+
+            while (number > 0)
+            {
+                Array.Resize(ref byteArray, byteArray.Length + 1);
+
+                byteArray[contor] = (byte)(number % 2);
+
+
+                contor += 1;
+
+                number = number / 2;
+
+
+            }
+
+            Array.Reverse(byteArray);
+
+            return byteArray;
+
+        }
+
+
+    }
+}
