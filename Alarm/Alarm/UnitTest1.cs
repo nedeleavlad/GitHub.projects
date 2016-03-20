ebd7c5ec61217313881264e2f1394ac39ea93a67
nedeleavlad <nedeleavlad28@outlook.com>
2016-03-20 21:10:03 +0200

    alarm enum, struct

---
+++ Alarm/Alarm/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Alarm
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public enum DaysOfWeek
+        {
+            Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+        }
+
+        private struct Alarm
+        {
+            public DaysOfWeek day;
+            public int hour;
+
+            public Alarm(DaysOfWeek day, int hour)
+            {
+                this.day = day;
+                this.hour = hour;
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
