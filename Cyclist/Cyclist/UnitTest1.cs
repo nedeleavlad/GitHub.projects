3cfdab095ba32e02224dec08cb2ca801bb5a244c
nedeleavlad <nedeleavlad28@outlook.com>
2016-03-21 13:24:11 +0200

    cyclist

--- Cyclist/Cyclist/UnitTest1.cs
+++ Cyclist/Cyclist/UnitTest1.cs
@@ -21,8 +21,46 @@ namespace Cyclist
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void CalculateTotalDistanceTraveled()
         {
+            var cyclist = new Cyclist[] { new Cyclist("Fritz", new int[] { 1, 2, 3 }, 3), new Cyclist("Rambo", new int[] { 1, 2, 8 }, 3), new Cyclist("Stalone", new int[] { 2, 3, 6 }, 3) };
+
+            Assert.AreEqual(263, GetTotalDistanceTraveledByAllCyclists(cyclist));
+        }
+
+        private double GetTotalDistanceTraveledByAllCyclists(Cyclist[] cyclists)
+        {
+            double result = 0;
+            for (int i = 0; i < cyclists.Length; i++)
+            {
+                result += GetTotalDistanceTraveledByOneCyclist(cyclists[i]);
+            }
+            return (int)result;
+        }
+
+        private static double GetTotalDistanceTraveledByOneCyclist(Cyclist cyclists)
+        {
+            int sum = 0;
+            sum = GetNumberOfRotations(cyclists);
+            return Math.PI * sum * cyclists.diameter;
+        }
+
+        [TestMethod]
+        public void CalculateNORotations()
+        {
+            var cyclist = new Cyclist("Iulian", new int[] { 1, 2, 5 }, 3);
+
+            Assert.AreEqual(8, GetNumberOfRotations(cyclist));
+        }
+
+        private static int GetNumberOfRotations(Cyclist cyclists)
+        {
+            int sum = 0;
+            for (int i = 0; i < cyclists.records.Length; i++)
+            {
+                sum += cyclists.records[i];
+            }
+            return sum;
         }
     }
 }
