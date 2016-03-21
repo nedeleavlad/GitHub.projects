f449988d77e494d027b8cc2eb49e607e4bd15241
nedeleavlad <nedeleavlad28@outlook.com>
2016-03-21 12:36:27 +0200

    public struct cyclist

---
+++ Cyclist/Cyclist/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Cyclist
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public struct Cyclist
+        {
+            public string name;
+            public int[] records;
+            public int diameter;
+
+            public Cyclist(string name, int[] records, int diameter)
+            {
+                this.name = name;
+                this.records = records;
+                this.diameter = diameter;
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
