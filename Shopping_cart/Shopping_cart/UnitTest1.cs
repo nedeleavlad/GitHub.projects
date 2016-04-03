e9400ccb12e5e86dcb53cfe70d549cd2bf0bd700
nedeleavlad <nedeleavlad28@outlook.com>
2016-04-03 22:51:57 +0300

    cheapest price

--- Shopping_cart/Shopping_cart/UnitTest1.cs
+++ Shopping_cart/Shopping_cart/UnitTest1.cs
@@ -37,5 +37,32 @@ namespace Shopping_cart
             }
             return result;
         }
+
+        [TestMethod]
+        public void CheapestPrice()
+        {
+            var product = new Product[] { new Product("Apple", 10), new Product("Milk", 25), new Product("Tomatoes", 15) };
+            Assert.AreEqual("Apple", GetTheCheaperProduct(product));
+        }
+
+        private string GetTheCheaperProduct(Product[] product)
+        {
+            string result = string.Empty;
+
+            if (product.Length == 0) return "Array is empty";
+
+            double counter = 0;
+
+            for (int i = 0; i < product.Length - 1; i++)
+            {
+                counter = product[i].price;
+                if (counter < product[i + 1].price)
+                {
+                    counter = product[i].price;
+                    result = product[i].name;
+                }
+            }
+            return result;
+        }
     }
 }
