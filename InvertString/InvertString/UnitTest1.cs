4ee9564563570efda06ff1e8b30b1c9abf9d6f96
nedeleavlad <nedeleavlad28@outlook.com>
2016-04-27 00:34:51 +0300

    invert string

---
+++ InvertString/InvertString/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InvertString
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestInvertString()
+        {
+            Assert.AreEqual("adfg", GetStringInverted("gfda"));
+        }
+
+        public string GetStringInverted(string givenString)
+        {
+            if (givenString.Length == 1) return givenString;
+
+            return givenString[givenString.Length - 1] + GetStringInverted(givenString.Substring(0, givenString.Length - 1));
+        }
+    }
+}
