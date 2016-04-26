8ae2df27474b4cbc706b5ab3ae58ec0832030d6a
nedeleavlad <nedeleavlad28@outlook.com>
2016-04-26 23:18:08 +0300

    fibonacci row

---
+++ fibonacci_row/fibonacci_row/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace fibonacci_row
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Fibonacci()
+        {
+            Assert.AreEqual(13, GetFibonacciRow(7));
+        }
+
+        public int GetFibonacciRow(int number)
+        {
+            if (number < 2) return number;
+
+            return (GetFibonacciRow(number - 1) + GetFibonacciRow(number - 2));
+        }
+    }
+}
