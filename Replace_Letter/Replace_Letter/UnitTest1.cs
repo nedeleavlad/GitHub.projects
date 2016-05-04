95800ea117e39b38c599fcf6eba2be58fe038f0c
nedeleavlad <nedeleavlad28@outlook.com>
2016-05-04 23:20:32 +0300

    letter replacement with another string

---
+++ Replace_Letter/Replace_Letter/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Replace_Letter
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ReplaceTest()
+        {
+            Assert.AreEqual("mccmcc", ChangeLetterWithString("mama", 'a', "cc"));
+        }
+
+        public string ChangeLetterWithString(string givenString, char letterToChange, string newString)
+        {
+            if (givenString.Length > 0)
+            {
+                if (givenString[0] == letterToChange) return newString + ChangeLetterWithString(givenString.Substring(1), letterToChange, newString);
+                return givenString[0] + ChangeLetterWithString(givenString.Substring(1), letterToChange, newString);
+            }
+            return givenString;
+        }
+    }
+}
