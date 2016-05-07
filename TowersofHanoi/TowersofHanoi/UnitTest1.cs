7f7704e2cef1b4676434eb588b9b5f118a57b4ce
nedeleavlad <nedeleavlad28@outlook.com>
2016-05-07 16:09:32 +0300

    towers of hanoi

---
+++ TowersofHanoi/TowersofHanoi/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TowersofHanoi
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Number_of_Moves()
+        {
+            Assert.AreEqual(7, GetNumberOfMoves(3));
+        }
+
+        [TestMethod]
+        public void NoMovesforOneDisk()
+        {
+            Assert.AreEqual(1, GetNumberOfMoves(1));
+        }
+
+        public int GetNumberOfMoves(int numberOfDisks)
+        {
+            if (numberOfDisks == 1) return 1;
+
+            return (2 * GetNumberOfMoves(numberOfDisks - 1)) + 1;
+        }
+    }
+}
