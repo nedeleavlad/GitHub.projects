87af19b60576b9137e4489278cc8ecfb6c7539cb
nedeleavlad <nedeleavlad28@outlook.com>
2016-05-19 00:07:45 +0300

    shopping cart product class

---
+++ ShoppingCartOOP/ShoppingCartOOP/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ShoppingCartOOP
+{
+    internal class Product
+    {
+        private string name;
+        private int price;
+        private int quantity;
+
+        public Product(string name, int price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public int Sum(int sum)
+        {
+            return sum = sum + this.price;
+        }
+
+        public int ComparePrices(Product Reference)
+        {
+            if (this.price < Reference.price)
+                return 1;
+            if (this.price > Reference.price)
+                return -1;
+            return 0;
+        }
+
+        public void IncreaseQuantity(int quantity)
+        {
+            this.quantity += quantity;
+        }
+
+        public void DecreaseQuantity(int quantity)
+        {
+            this.quantity -= quantity;
+        }
+
+        public int Total(int price, int quantity)
+        {
+            return this.quantity * this.price;
+        }
+    }
+}
