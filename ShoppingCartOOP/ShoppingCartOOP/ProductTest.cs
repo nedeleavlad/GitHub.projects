aba67edbaa8bba5b5b8b855b7728968b5c491b0c
nedeleavlad <nedeleavlad28@outlook.com>
2016-05-19 00:24:00 +0300

    product test class

--- ShoppingCartOOP/ShoppingCartOOP/ProductTest.cs
+++ ShoppingCartOOP/ShoppingCartOOP/ProductTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace ShoppingCartOOP
 {
-    class ProductTest
+    [TestClass]
+    public class Productt
     {
+        [TestMethod]
+        public void TotalCosts()
+        {
+            Product pp = new Product("milk", 7, 2);
+            Assert.AreEqual(14, pp.Total(7, 2));
+        }
+
+        [TestMethod]
+        public void Summ()
+        {
+            Product pp = new Product("milk", 5, 1);
+            Product pp1 = new Product("bread", 6, 1);
+            int a = 5;
+            Assert.AreEqual(10, pp.Sum(a));
+        }
     }
 }
