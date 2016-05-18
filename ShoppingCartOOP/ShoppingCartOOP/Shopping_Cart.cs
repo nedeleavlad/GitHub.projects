7d1b5fa4bd8d522ed20486eb1d9a47bd97e092b8
nedeleavlad <nedeleavlad28@outlook.com>
2016-05-19 01:02:23 +0300

    shopping cart class

---
+++ ShoppingCartOOP/ShoppingCartOOP/Shopping_Cart.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartOOP
+{
+    internal class Shopping_Cart
+    {
+        public Product[] products = new Product[0];
+
+        public void Add(Product product)
+        {
+            Array.Resize(ref products, products.Length + 1);
+            products[products.Length - 1] = product;
+        }
+
+        public int GetSumofProducts()
+        {
+            int sum = 0;
+            foreach (var product in products)
+            {
+                sum = product.Sum(sum);
+            }
+            return sum;
+        }
+
+        public double CalculateTheAveragePrice()
+        {
+            return GetSumofProducts() / products.Length;
+        }
+
+        public Product GetCheapestProduct()
+        {
+            var index = 0;
+            for (int i = 1; i < products.Length; i++)
+            {
+                if (products[index].ComparePrices(products[i]) == -1)
+                    index = i;
+            }
+            return products[index];
+        }
+
+        public void RemoveSpecificPosition(int index)
+        {
+            for (int i = index; i < products.Length - 1; i++)
+                products[i] = products[i + 1];
+            Array.Resize(ref products, products.Length - 1);
+        }
+
+        public void RemoveAProduct()
+        {
+            int index = GetMostExpenive();
+            RemoveSpecificPosition(index);
+        }
+
+        public int GetMostExpenive()
+        {
+            var index = 0;
+            for (int i = 1; i < products.Length; i++)
+            {
+                if (products[index].ComparePrices(products[i]) == 1)
+                    index = i;
+            }
+            return index;
+        }
+    }
+}
---
+++ ShoppingCartOOP/ShoppingCartOOP/Shopping_CartTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartOOP
+{
+    [TestClass]
+    public class Shopping_Cartt
+    {
+        private Shopping_Cart shoppingCart;
+        private Product[] products = new Product[0];
+
+        [TestMethod]
+        public void SumPrices()
+        {
+            shoppingCart = new Shopping_Cart(); ;
+            Product a = new Product("appel", 4, 1);
+            Product b = new Product("orange", 1, 1);
+            Product c = new Product("sugar", 3, 1);
+            shoppingCart.Add(new Product("appel", 4, 1));
+            shoppingCart.Add(new Product("orange", 1, 1));
+            shoppingCart.Add(new Product("sugar", 3, 1));
+            Assert.AreEqual(8, shoppingCart.GetSumofProducts());
+        }
+
+        [TestMethod]
+        public void RemoveElementPosX()
+        {
+            shoppingCart = new Shopping_Cart();
+            shoppingCart.Add(new Product("appel", 4, 1));
+            shoppingCart.Add(new Product("orange", 1, 1));
+            shoppingCart.Add(new Product("sugar", 1, 1));
+            shoppingCart.Add(new Product("abb", 5, 1));
+            shoppingCart.RemoveSpecificPosition(1);
+            Assert.AreEqual(10, shoppingCart.GetSumofProducts());
+        }
+
+        [TestMethod]
+        public void MostExpensive()
+        {
+            shoppingCart = new Shopping_Cart();
+            shoppingCart.Add(new Product("appel", 4, 1));
+            shoppingCart.Add(new Product("orange", 1, 1));
+            shoppingCart.Add(new Product("sugar", 7, 1));
+            shoppingCart.Add(new Product("arrr", 5, 1));
+            Assert.AreEqual(2, shoppingCart.GetMostExpenive());
+        }
+    }
+}
