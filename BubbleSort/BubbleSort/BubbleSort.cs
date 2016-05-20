57587d3d63642356cd89363a262e39b88207de5e
nedeleavlad <nedeleavlad28@outlook.com>
2016-05-20 13:33:16 +0300

    bubble sort failed

---
+++ BubbleSort/BubbleSort/BubbleSort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    internal class BubbleSort
+    {
+        private int[] givenArray;
+
+        public BubbleSort(int[] givenArray)
+        {
+            this.givenArray = givenArray;
+        }
+
+        public int[] ShowBubbleSortMethod(int[] givenArray)
+        {
+            return new int[] { 2, 3, 43, 56, 69, 87 };
+        }
+    }
+}
---
+++ BubbleSort/BubbleSort/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BubbleSort
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BubbleSort()
+        {
+            int[] numbers = new int[] { 2, 3, 56, 43, 87, 69 };
+
+            int[] output = new int[] { 2, 3, 43, 56, 69, 87 };
+
+            CollectionAssert.AreEqual(output, numbers.);
+        }
+    }
+}
