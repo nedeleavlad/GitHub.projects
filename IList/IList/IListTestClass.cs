eb90b12d33c707b90cb5b9461e5bc6efd108263c
nedeleavlad <nedeleavlad28@outlook.com>
2016-05-25 23:28:12 +0300

    partial ILIST

---
+++ IList/IList/IListTestClass.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace IList
+{
+    [TestClass]
+    public class IListTestClass
+    {
+        [TestMethod]
+        public void IndexOf()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Assert.AreEqual(1, list.IndexOf(2));
+        }
+
+        [TestMethod]
+        public void InsertInListAnInt()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Insert(2, 10);
+            list.Insert(2, 10);
+            list.Insert(2, 10);
+            list.Insert(2, 10);
+            Assert.AreEqual(7, list.Count);
+        }
+
+        [TestMethod]
+        public void Add()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(3);
+            Assert.AreEqual(4, list.Count);
+        }
+
+        [TestMethod]
+        public void RemovefromList()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.RemoveAt(1);
+            Assert.AreEqual(2, list.Count);
+        }
+
+        [TestMethod]
+        public void ClearList()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Clear();
+            Assert.AreEqual(0, list.Count);
+        }
+    }
+}
---
+++ IList/IList/List.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IList
+{
+    internal class List<T> : IList<T>
+    {
+        private T[] array = new T[] { };
+        private int _index;
+
+        public List()
+        {
+            this.array = array;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index < Count && index >= 0)
+            {
+                if (_index <= array.Length)
+                    Array.Resize(ref array, array.Length * 2);
+                _index++;
+                for (int i = Count - 1; i > index; i--)
+                {
+                    array[i] = array[i - 1];
+                }
+                array[index] = item;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _index;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if ((index >= 0) && (index < Count))
+            {
+                for (int i = index; i < Count - 1; i++)
+                {
+                    array[i] = array[i + 1];
+                }
+                _index--;
+            }
+        }
+
+        public int IndexOf(T item)
+        {
+            int itemIndex = -1;
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i].Equals(item))
+                {
+                    itemIndex = i;
+                    break;
+                }
+            }
+            return itemIndex;
+        }
+
+        public void Add(T item)
+        {
+            if (Count == 0)
+                Array.Resize(ref array, array.Length + 1);
+            if (array.Length <= Count && _index == Count)
+                Array.Resize(ref array, array.Length * 2);
+
+            array[_index] = item;
+            _index++;
+        }
+
+        public void Clear()
+        {
+            _index = 0;
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(T item)
+        {
+            return true;
+        }
+    }
+}
