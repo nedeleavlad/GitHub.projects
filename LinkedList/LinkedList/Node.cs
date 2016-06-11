c30450745c8ea0efd2898a132f6c23521b268d26
nedeleavlad <nedeleavlad28@outlook.com>
2016-06-11 11:42:55 +0300

    node class

---
+++ LinkedList/LinkedList/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Node<T>
+    {
+        private T value;
+
+        private Node<T> next;
+
+        public Node(T start)
+        {
+            value = start;
+
+            next = null;
+        }
+    }
+}
---
+++ LinkedList/LinkedList/TestClass.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LinkedList
+{
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
