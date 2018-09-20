838c4c31f9b4349c2470b54f85847c1e1d74e13c
Nedelea Vlad <nedeleavlad28@outlook.com>
2018-09-20 12:54:33 +0300

    xunit chrome

--- XUnitTestProject1/XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/XUnitTestProject1/UnitTest1.cs
@@ -2,83 +2,101 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using Xunit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using System.Threading;
-using Microsoft.VisualBasic;
-
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
 
 namespace XUnitTestProject1
 {
+
     public class UnitTest1
     {
+        private const string userInputId = "textfield-1013-inputEl";
+        private const string passInputId = "textfield-1014-inputEl";
+        private const string logInId = "button-1016-btnInnerEl";
+
+        private readonly IWebElement username;
+        private readonly IWebElement pass;
+        private readonly ChromeDriver driver;
+        private readonly IWebElement submit;
+
+        public UnitTest1()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("configs.json")
+                .Build();
+
+            //Given
+            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Url = config["url"];
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            username = wait.Until(drv => drv.FindElement(By.Id(userInputId)));
+            pass = wait.Until(drv => drv.FindElement(By.Id(passInputId)));
+            submit = wait.Until(drv => drv.FindElement(By.Id(logInId)));
+        }
+
         [Fact]
         public void LoginSucceed()
-        {
-            var driver = new ChromeDriver(@"D:\Vladuuuut\GITHUB\JuniorMind_projects\XUnitTestProject1\XUnitTestProject1\bin\Debug\netcoreapp2.0");
-            driver.Url= "http://win-h96lo6s37fc/";
-            Thread.Sleep(5000);
-
-            IWebElement username = driver.FindElement(By.Id("textfield-1013-inputEl"));
+        {   //When
             username.SendKeys("sa");
-
-            IWebElement pass = driver.FindElement(By.Id("textfield-1014-inputEl"));
             pass.SendKeys("sa");
+            submit.Click();
 
-             var click = driver.FindElement(By.Id("button-1016-btnInnerEl"));
-             click.Click();
+            WebDriverWait waitLogIn = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            waitLogIn.Until(drv => drv.FindElement(By.Id("button-1030-btnIconEl")));
 
-            driver.Quit();
+
+            //Then
+            Assert.True(driver.FindElement(By.Id("button-1041-btnIconEl")).Displayed);
         }
 
 
         [Fact]
         public void LoginFail()
-        {
-            var driver = new ChromeDriver(@"D:\Vladuuuut\GITHUB\JuniorMind_projects\XUnitTestProject1\XUnitTestProject1\bin\Debug\netcoreapp2.0");
-            driver.Url = "http://win-h96lo6s37fc/";
-            Thread.Sleep(5000);
-
-            IWebElement username = driver.FindElement(By.Id("textfield-1013-inputEl"));
+        {   //When
             username.SendKeys("sasasass");
-
-            IWebElement pass = driver.FindElement(By.Id("textfield-1014-inputEl"));
             pass.SendKeys("sa");
+            submit.Click();
 
-            var click = driver.FindElement(By.Id("button-1016-btnInnerEl"));
-            click.Click();
+            WebDriverWait waitLogIn = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            waitLogIn.Until(drv => drv.FindElement(By.Id("messagebox-1001_header-title-textEl")));
 
-
-            driver.Quit();
+            //Then
+            Assert.True(driver.FindElement(By.Id("messagebox-1001_header-title-textEl")).Displayed);
         }
 
         [Fact]
         public void FillWithSpecialCharacters()
-        {
-            var driver = new ChromeDriver(@"D:\Vladuuuut\GITHUB\JuniorMind_projects\XUnitTestProject1\XUnitTestProject1\bin\Debug\netcoreapp2.0");
-            driver.Url = "http://win-h96lo6s37fc/";
-            Thread.Sleep(5000);
-
-            IWebElement username = driver.FindElement(By.Id("textfield-1013-inputEl"));
+        { 
+            //When
             username.SendKeys("sasasass??????####%%%%");
-
-            IWebElement pass = driver.FindElement(By.Id("textfield-1014-inputEl"));
             pass.SendKeys("%%%%ggg>>");
+            submit.Click();
+            WebDriverWait waitLogIn = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            waitLogIn.Until(drv => drv.FindElement(By.Id("messagebox-1001_header-title-textEl")));
 
-            var click = driver.FindElement(By.Id("button-1016-btnInnerEl"));
-            click.Click();
-
-            driver.Quit();
+            //Then
+            Assert.True(driver.FindElement(By.Id("messagebox-1001_header-title-textEl")).Displayed);
         }
 
-       
+        [Fact]
+        public void LargeNumberOfCharacters()
+        {   
+            //When
+            string st = new string('t', 1000);
+            username.SendKeys(st);
+            pass.SendKeys("%%%%ggg>>");
+            submit.Click();
 
+            WebDriverWait waitLogIn = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            waitLogIn.Until(drv => drv.FindElement(By.Id("messagebox-1001_header-title-textEl")));
 
+            //Then
+            Assert.True(driver.FindElement(By.Id("messagebox-1001_header-title-textEl")).Displayed);
+        }
     }
 }
